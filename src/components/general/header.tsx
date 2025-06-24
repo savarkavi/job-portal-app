ce507374a46ca9884fdf3c0a477c81db05dd8228
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-06-24 19:07:53 +0530

    feat: refactor Header component to conditionally render login/signup buttons based on user authentication state

--- src/components/general/header.tsx
+++ src/components/general/header.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Header = () => {
   return (
     <div className="flex items-center justify-between border-b p-4">
       <h2 className="font-serif text-2xl font-bold">bigshot</h2>
-      <div className="flex items-center gap-4">
-        <Link href="/sign-in">
-          <Button variant="outline">Log In</Button>
-        </Link>
-        <Link href="/sign-up">
-          <Button>Sign Up</Button>
-        </Link>
-      </div>
+      <SignedOut>
+        <div className="flex items-center gap-4">
+          <Link href="/sign-in">
+            <Button variant="outline">Log In</Button>
+          </Link>
+          <Link href="/sign-up">
+            <Button>Sign Up</Button>
+          </Link>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 };
