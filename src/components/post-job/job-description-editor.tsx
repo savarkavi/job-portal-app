77d2653b23922216b165653897686700fe4d1685
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-07-04 20:02:45 +0530

    feat: enhance JobDescriptionEditor with list functionality and integrate form handling

--- src/components/post-job/job-description-editor.tsx
+++ src/components/post-job/job-description-editor.tsx
@@ -6,24 +6,44 @@ import EditorMenuBar from "./editor-menu-bar";
 import Underline from "@tiptap/extension-underline";
 import TextAlign from "@tiptap/extension-text-align";
 import Heading from "@tiptap/extension-heading";
+import ListItem from "@tiptap/extension-list-item";
+import OrderedList from "@tiptap/extension-ordered-list";
+import BulletList from "@tiptap/extension-bullet-list";
 import "./styles.css";
+import { ControllerRenderProps } from "react-hook-form";
+import { JobPostFormValues } from "@/lib/types";
 
-const JobDescriptionEditor = () => {
+interface JobDescriptionEditorProps {
+  field: ControllerRenderProps<JobPostFormValues, "jobDescription">;
+}
+
+const JobDescriptionEditor = ({ field }: JobDescriptionEditorProps) => {
   const editor = useEditor({
     extensions: [
       StarterKit,
       Underline,
+      ListItem,
+      BulletList,
+      OrderedList,
       TextAlign.configure({ types: ["heading", "paragraph"] }),
       Heading.configure({ levels: [1, 2, 3, 4] }),
     ],
-    content: "<p>Hello World! 🌎️</p>",
     immediatelyRender: false,
+    editorProps: {
+      attributes: {
+        class: "focus:outline-none min-h-[300px] prose",
+      },
+    },
+    onUpdate: ({ editor }) => {
+      field.onChange(JSON.stringify(editor.getJSON()));
+    },
+    content: field.value ? JSON.parse(field.value) : "",
   });
 
   return (
     <div className="mt-2 min-h-[500px] rounded-xl border">
       <EditorMenuBar editor={editor} />
-      <div className="mt-2 p-4">
+      <div className="prose p-4">
         <EditorContent editor={editor} />
       </div>
     </div>
