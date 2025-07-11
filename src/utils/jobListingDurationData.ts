4513ff1e6be16aee4d45b5f01253f7b656879f41
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-07-11 10:29:14 +0530

    update job listing duration prices to numeric format

--- src/utils/jobListingDurationData.ts
+++ src/utils/jobListingDurationData.ts
@@ -4,20 +4,20 @@ export const jobLisitngDurationData = [
     label: "30 Days",
     days: 30,
     description: "Standard listing",
-    price: "₹999",
+    price: 99,
   },
   {
     id: "60-days",
     label: "60 Days",
     days: 60,
     description: "Extended visibility",
-    price: "₹1999",
+    price: 199,
   },
   {
     id: "90-days",
     label: "90 Days",
     days: 90,
     description: "Maximum exposure",
-    price: "₹2999",
+    price: 299,
   },
 ];
